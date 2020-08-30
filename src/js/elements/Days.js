1e6bc8e99378d9d98135247369da3102df8db662
mathis <mathis.lucka@gmail.com>
2020-08-30 22:17:29 +0200

    highlight current date

--- src/js/elements/Days.js
+++ src/js/elements/Days.js
@@ -1,6 +1,6 @@
 import { createDateFromArray } from '../utils/DateManipulation.js'
 
-function createButton(day, month, year, localMonth, weekday, isEnabled) {
+function createButton(day, month, year, localMonth, weekday, isEnabled, isToday) {
     var btn = document.createElement('button')
     var txt = document.createTextNode(day)
     btn.value = year +'$' + month + '$' + day
@@ -9,6 +9,7 @@ function createButton(day, month, year, localMonth, weekday, isEnabled) {
     btn.setAttribute('data-ad-id', '' + day + month + year)
     btn.setAttribute('aria-label', weekday + ' ' + day + ' ' + localMonth + ' ' + year)
     btn.setAttribute('aria-disabled', !isEnabled)
+    isToday && btn.classList.add('a1-is-today')
     btn.appendChild(txt)
     return btn
 }
@@ -17,8 +18,8 @@ function configureDays (month, year, localMonth, rangeChecker) {
     return function (day, weekday) {
         var cell = document.createElement('td')
         var date = createDateFromArray([year, month, day])
-        date.setHours(0,0,0,0) == (new Date()).setHours(0,0,0,0) && cell.classList.add('ad-is-today')
-        cell.appendChild(createButton(day, month, year, localMonth, weekday, rangeChecker(date)))
+        var isToday = date.setHours(0,0,0,0) == (new Date()).setHours(0,0,0,0)
+        cell.appendChild(createButton(day, month, year, localMonth, weekday, rangeChecker(date), isToday))
         return cell
     }
 }
